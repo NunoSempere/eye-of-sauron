36e6951f56202deb69530ee85185edac91e67e4b
NunoSempere <nuno.semperelh@protonmail.com>
2025-04-19 21:24:57 -0300

    add CNN

---
+++ server/sources/potpourri/fetchCNN.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/xml"
+	"git.nunosempere.com/NunoSempere/news/lib/types"
+	"git.nunosempere.com/NunoSempere/news/lib/web"
+	"log"
+	"time"
+)
+
+type RSSFeed struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+	Items []Item `xml:"item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	PubDate     string `xml:"pubDate"`
+	Description string `xml:"description"`
+}
+
+var cnnFeeds = map[string]string{
+	"top":        "http://rss.cnn.com/rss/cnn_topstories.rss",
+	"world":      "http://rss.cnn.com/rss/cnn_world.rss",
+	"us":         "http://rss.cnn.com/rss/cnn_us.rss",
+	"politics":   "http://rss.cnn.com/rss/cnn_allpolitics.rss",
+	"technology": "http://rss.cnn.com/rss/cnn_tech.rss",
+	"latest": "http://rss.cnn.com/rss/cnn_latest.rss",
+}
+
+func FetchCNNFeed(feedURL string) ([]types.Source, error) {
+	log.Printf("Fetching CNN feed: %s", feedURL)
+	
+	xml_bytes, err := web.Get(feedURL)
+	if err != nil {
+		return nil, err
+	}
+
+	var feed RSSFeed
+	err = xml.Unmarshal(xml_bytes, &feed)
+	if err != nil {
+		log.Printf("Error unmarshaling XML: %v\n", err)
+		return nil, err
+	}
+
+	var sources []types.Source
+	for _, item := range feed.Channel.Items {
+		// Parse the date to ensure it's fresh
+		pubDate, err := time.Parse(time.RFC1123, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing date %s: %v", item.PubDate, err)
+			continue
+		}
+
+		// Only include articles from the last 24 hours
+		if time.Since(pubDate) > 24*time.Hour {
+			continue
+		}
+
+		sources = append(sources, types.Source{
+			Title: item.Title,
+			Link:  item.Link,
+			Date:  pubDate.Format(time.RFC3339),
+		})
+	}
+
+	return sources, nil
+}
+
+func FetchAllCNNFeeds() ([]types.Source, error) {
+	var allSources []types.Source
+	
+	for feedName, feedURL := range cnnFeeds {
+		log.Printf("Processing CNN %s feed", feedName)
+		sources, err := FetchCNNFeed(feedURL)
+		if err != nil {
+			log.Printf("Error fetching CNN %s feed: %v", feedName, err)
+			continue
+		}
+		allSources = append(allSources, sources...)
+	}
+
+	return allSources, nil
+}
---
+++ server/sources/potpourri/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"os"
+	"io"
+	"time"
+
+	"github.com/joho/godotenv"
+	"git.nunosempere.com/NunoSempere/news/lib/types"
+)
+
+func main() {
+	// Set up logging
+	logFile, err := os.OpenFile("sources/potpourri/v2.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer logFile.Close()
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
+
+	// Load environment variables
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	openai_key := os.Getenv("OPENAI_KEY")
+	pg_database_url := os.Getenv("DATABASE_POOL_URL")
+
+	for {
+		log.Println("Starting potpourri sources processing")
+		
+		// Process CNN feeds
+		log.Println("Processing CNN feeds")
+		cnnSources, err := FetchAllCNNFeeds()
+		if err != nil {
+			log.Printf("Error fetching CNN feeds: %v", err)
+		} else {
+			log.Printf("Found %d CNN articles", len(cnnSources))
+			for i, source := range cnnSources {
+				log.Printf("\nProcessing CNN article %d/%d: %s", i+1, len(cnnSources), source.Title)
+				expanded_source, passes_filters := FilterAndExpandSource(source, openai_key, pg_database_url)
+				if passes_filters {
+					expanded_source.Origin = "CNN"
+					SaveSource(expanded_source)
+				}
+			}
+		}
+		
+		log.Printf("Finished processing potpourri sources, sleeping for 1 hour")
+		time.Sleep(1 * time.Hour)
+	}
+}
