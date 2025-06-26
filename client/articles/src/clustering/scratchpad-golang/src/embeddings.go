ef1cfec32999c2d0ae6b2be301c321889770983f
NunoSempere <nuno.semperelh@protonmail.com>
2025-06-26 18:15:14 -0500

    get embeddings all at once

--- client/articles/src/clustering/scratchpad-golang/src/embeddings.go
+++ client/articles/src/clustering/scratchpad-golang/src/embeddings.go
@@ -6,39 +6,32 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 )
 
-func getEmbedding(s string, token string) ([]float64, error) {
+func GetEmbeddings(texts []string, token string) ([][]float64, error) {
+	
 	client := openai.NewClient(token)
 
 	// Create an EmbeddingRequest for the user query
 	queryReq := openai.EmbeddingRequest{
-		Input: []string{s},
-		Model: openai.AdaEmbeddingV2,
+		Input: texts,
+		Model: openai.SmallEmbedding3,
 	}
 
 	// Create an embedding for the user query
 	queryResponse, err := client.CreateEmbeddings(context.Background(), queryReq)
 	if err != nil {
-		return []float64{}, err
+		return [][]float64{}, err
 	}
-	queryEmbedding := queryResponse.Data[0]
-	f32s := queryEmbedding.Embedding
-	f64s := make([]float64, len(f32s))
-	// Convert each float32 to float64
-	for i, v := range f32s {
-		f64s[i] = float64(v)
-	}
-	return f64s, nil
-}
 
-func GetEmbeddings(texts []string, token string) ([][]float64, error) {
 	es := [][]float64{}
-	for _, t := range texts {
-		e, err := getEmbedding(t, token)
-		if err != nil {
-			return [][]float64{}, err
+	for i, e := range queryResponse.Data {
+		f32s := e.Embedding
+		f64s := make([]float64, len(f32s))
+		for i, v := range f32s {
+			f64s[i] = float64(v)
 		}
-		fmt.Printf("Embedding for %s is [%f, %f, %f, ...]\n", t, e[0], e[1], e[2])
-		es = append(es, e)
+		es = append(es, f64s)
+		fmt.Printf("Embedding for %s is [%f, %f, %f, ...]\n", texts[i], f64s[0], f64s[1], f64s[2])
 	}
+
 	return es, nil
 }
