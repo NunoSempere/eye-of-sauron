8033903e6d5821f4f98130728fa72fb624e84793
NunoSempere <nuno.semperelh@protonmail.com>
2025-06-26 18:45:24 -0500

    amove stuff to clustering package

---
+++ client/articles/src/cluster/titles.go
@@ -0,0 +1,35 @@
+package cluster
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func ReadFileLines(filename string) ([]string, error) {
+	var lines []string
+
+	// Open the file for reading
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a new scanner to read the file
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		clean := strings.ReplaceAll(line, "<b>", "")
+		clean = strings.ReplaceAll(line, "</b>", "")
+		clean = strings.ReplaceAll(line, "&#39;", "'")
+		lines = append(lines, clean) // Append each line to the slice
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
